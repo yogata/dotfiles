b4abe3526d3304af850c5f1d444df10cb2f3415f
Ogata Yasuhiko <ogataysa@intellilink.co.jp>
2018-03-05 13:16:10 +0900

    add pause command

--- vimfiles/01_symlink_admin_required.bat
+++ vimfiles/01_symlink_admin_required.bat
@@ -1,5 +1,8 @@
-mklink %USERPROFILE%\Documents\ogataysa\901_Tools\vim80\_vimrc       %USERPROFILE%\Documents\ogataysa\902_github\dotfiles\vimfiles\_vimrc
-mklink %USERPROFILE%\Documents\ogataysa\901_Tools\vim80\_vimrc_local %USERPROFILE%\Documents\ogataysa\902_github\dotfiles\vimfiles\_vimrc_local
-mklink %USERPROFILE%\Documents\ogataysa\901_Tools\vim80\_gvimrc      %USERPROFILE%\Documents\ogataysa\902_github\dotfiles\vimfiles\_gvimrc
-mklink /D %USERPROFILE%\.vim %USERPROFILE%\Documents\ogataysa\902_github\dotfiles\vimfiles
+set VIM=%USERPROFILE%\Documents\ogataysa\901_Tools\vim80
+set DOTFILES=%USERPROFILE%\Documents\ogataysa\dotfiles.git\vimfiles
+mklink %VIM%\_vimrc       %DOTFILES%\_vimrc
+mklink %VIM%\_vimrc_local %DOTFILES%\_vimrc_local
+mklink %VIM%\_gvimrc      %DOTFILES%\_gvimrc
+mklink /D %USERPROFILE%\.vim %DOTFILES%
 
+pause
