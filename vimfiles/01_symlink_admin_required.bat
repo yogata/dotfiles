e1a32c056b6139866e887d1bdea5cf74ba48ca08
Ogata Yasuhiko <ogata.yasuhiko@gmail.com>
2023-10-14 20:07:03 +0900

    update dotfiles dirname

--- vimfiles/01_symlink_admin_required.bat
+++ vimfiles/01_symlink_admin_required.bat
@@ -1,5 +1,5 @@
 set VIM=%USERPROFILE%\Documents\001_Software\Editor\vim82-kaoriya-win64
-set DOTFILES=%USERPROFILE%\work\dotfiles.git\vimfiles
+set DOTFILES=%USERPROFILE%\work\dotfiles\vimfiles
 mklink %VIM%\_vimrc       %DOTFILES%\_vimrc
 mklink %VIM%\_vimrc_local %DOTFILES%\_vimrc_local
 mklink %VIM%\_gvimrc      %DOTFILES%\_gvimrc
